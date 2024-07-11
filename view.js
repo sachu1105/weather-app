54683bab77d1e25aa712114d2fa0c8ba9f338f59
sachu <sachusajith170@gmail.com>
2024-07-11 21:59:19 +0530

    added if else to show erro msg when wrong city enter

--- view.js
+++ view.js
@@ -1,4 +1,4 @@
-const apiKey = "11ee96f56a9301b47edfff4e5b53d58d";
+const apiKey = "*";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".card input");
@@ -7,25 +7,35 @@ const weatherIcon = document.querySelector(".imgweather img");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
-    console.log(data);
 
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    document.querySelector(".cityName").innerHTML = data.name;
-    document.querySelector(".weather").innerHTML = data.weather[0].main;
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block";
+        
+    }
+    else{
+        var data = await response.json();
+    
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src="./images/Cloudy.jpg";
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".cityName").innerHTML = data.name;
+        document.querySelector(".weather").innerHTML = data.weather[0].main;
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src="./images/Cloudy.jpg";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src="./images/sunny.avif";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src="./images/Rainy.jpg"
+        }
+        else if(data.weather[0].main == "thunderstorm"){
+            weatherIcon.src="./images/Thunder-Storm.jpg"
+        }
+        document.querySelector(".error").style.display = "none";
+
     }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src="./images/sunny.avif";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src="./images/Rainy.jpg"
-    }
-    else if(data.weather[0].main == "thunderstorm"){
-        weatherIcon.src="./images/thunderstorm.jpg"
-    }
+   
 
 }
 
